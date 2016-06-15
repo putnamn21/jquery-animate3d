82d57878497d194974217d21abea3b360d6e04c2
putnamn21 <nathanputnam2@gmail.com>
2016-06-15 11:09:01 -0600

    Final Update
    Added download button and view on github, icons, etc.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
    
    $('.animate-1').animate3d({});
-
-   var data = $('.active').children('a').data('option');
-   $('.animate-2').animate3d(data);
+   
+   run();
+   
    
 $('.nav-pills li').click(function(){
    
    $('.active').removeClass('active');
-   
-   $(this).addClass('active');
-   
+   $(this).addClass('active'); 
+   run();
+})
+
+function run () {
    var data = $('.active').children('a').data('option');
    $('.animate-2').animate3d(data);
-})
-   
+   $('#code').text("$('element').animate3d("+ JSON.stringify(data) +")");
+};
    
 });
